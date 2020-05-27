75376dac33202bc52c42e3ab2f6f1a82721d865d
org <chai1919@outlook.com>
2020-05-27 20:59:55 +0800

    '任务完成'

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <!-- <Add :addComments="addComments" /> -->
+      <Add ref="add" />
+      <!-- <Add @addComments="addComments" /> -->
+      <List :comments="comments" :deleteComments="deleteComments" />
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Add from "@/components/Add";
+import List from "@/components/List";
+export default {
+  components: {
+    Add,
+    List
+  },
+  mounted() {
+    this.$refs.add.$on("addComments", this.addComments);
+  },
+  data() {
+    return {
+      comments: [
+        { id: 1, username: "张三", text: "vue挺好的" },
+        { id: 2, username: "李四", text: "vue不错的呦" },
+        { id: 3, username: "王五", text: "vue太棒了" }
+      ]
+    };
+  },
+  methods: {
+    addComments(obj) {
+      this.comments.unshift(obj);
+    },
+    deleteComments(index) {
+      this.comments.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,19 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');//打包带着html文件
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');//每次打包自动清理dist
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
     // entry: path.resolve(__dirname,'src/index.js'),//入口
-    entry:'./src/index.js',//入口
+    entry: './src/index.js',//入口
 
     output: {
-      path: path.resolve(__dirname, 'dist'),//打包完成后的文件放在哪，dist文件夹会自动创建好
-      filename: 'main.js'
+        path: path.resolve(__dirname, 'dist'),//打包完成后的文件放在哪，dist文件夹会自动创建好
+        filename: 'main.js'
     },
 
     //配置各种loader
-    module:{
-        rules:[
+    module: {
+        rules: [
             //解析ES6
             {
                 test: /\.js$/,
@@ -32,7 +32,7 @@ module.exports = {
             //打包CSS vue-style-loader是style-loader的一个增强版本
             {
                 test: /\.css$/,
-                use: [ 'vue-style-loader', 'css-loader' ]//使用loader的时候有顺序，从后往前
+                use: ['vue-style-loader', 'css-loader']//使用loader的时候有顺序，从后往前
             },
             //打包图片，内部会使用到file-loader
             {
@@ -41,9 +41,9 @@ module.exports = {
                     {
                         loader: 'url-loader',//内部会使用到file-loader
                         options: {
-                            limit: 8192*100, 
+                            limit: 8192 * 100,
                             //如果图片小于这个值，会被base64编码为一个字符串，提高效率，减少请求
-                            name:'[hash:8].[ext]' //打包后的图片名字，截取哈希值的前八位就ok
+                            name: '[hash:8].[ext]' //打包后的图片名字，截取哈希值的前八位就ok
                         }
                     }
                 ]
@@ -57,27 +57,38 @@ module.exports = {
     },
 
     //配置插件完成其它搞不定的功能
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
             //得去找我要配置的html文件
-            template:'./src/public/index.html'
+            template: './src/public/index.html'
         }),
         new CleanWebpackPlugin(),//自动清理dist文件夹插件,
-        new VueLoaderPlugin()//vue相关插件
+        new VueLoaderPlugin(),//vue相关插件
+        new CopyPlugin([
+            {
+                from: path.resolve(__dirname, 'src/public'),
+                to: path.resolve(__dirname, 'dist'),
+                ignore: ['index.html']
+            }
+        ]),
     ],
 
-    mode:'development',//配置启动模式，开发模式还是生产模式
+    mode: 'development',//配置启动模式，开发模式还是生产模式
 
     devServer: {
-        port:8080,//服务启动的端口
-        open:true,//是否自动打开浏览器
-        quiet:true,//输出少量的提示信息
+        port: 8080,//服务启动的端口
+        open: true,//是否自动打开浏览器
+        quiet: true,//输出少量的提示信息
     },
 
-    devtool:'cheap-module-eval-source-map',//定位出错所在的原始代码行
+    devtool: 'cheap-module-eval-source-map',//定位出错所在的原始代码行
 
-    resolve:{
-        extensions: [".js", ".json",".vue"] //解决导入省略后缀名称
+    resolve: {
+        extensions: [".js", ".json", ".vue"], //解决导入省略后缀名称
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': path.resolve(__dirname, 'src')
+        }
     }
 
-  };
+};
